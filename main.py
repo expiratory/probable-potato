90978399339df4e858a71490f9efec2f7dee1358
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-15 23:19:18 +0300

    add some programms

---
+++ main.py
@@ -0,0 +1,12 @@
+# задача 2
+# number = int(input("Введите число: "))
+# while(number < 0 or number > 10):
+#     number = int(input("Введите число в диапазоне от 0 до 10: "))
+# print(number**2)
+#
+# задача 1
+# number = int(input("Введите число: "))
+# number = number + 2
+# print(number)
+#
+#
