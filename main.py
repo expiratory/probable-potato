832b510152dd6c7c0e73a9489313fb88a4d0a969
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-16 00:40:52 +0300

    that's all, folks

--- main.py
+++ main.py
@@ -9,4 +9,23 @@
 # number = number + 2
 # print(number)
 #
+#задача 3
+# name = input("Введите ваше имя и фамилию: ")
+# age = int(input("Введите ваш возраст: "))
+# weight = int(input("Введите ваш вес: "))
+# if age < 30:
+#     if weight >= 50 and weight <=120:
+#         print(name, "Ваше состояние хорошее")
+#     else:
+#         print("А про вас в задаче не указано)")
+# if age > 30 and age < 40:
+#     if weight <= 50 or weight >= 120:
+#         print(name, "Вам следует заняться собой")
+#     else:
+#         print("А про вас в задаче не указано)")
+# if age > 40:
+#     if weight <= 50 or weight >= 120:
+#         print(name, "Вам требуется врачебный осмотр")
+#     else:
+#         print("А про вас в задаче не указано)")
 #
