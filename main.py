527402900762d1d4d09e25f32e25b16646e0c486
Aleksandr_Kilchevskiy <alexkill94@mail.ru>
2022-10-16 12:25:19 +0300

    add hot or cold

--- main.py
+++ main.py
@@ -29,3 +29,33 @@
 #     else:
 #         print("А про вас в задаче не указано)")
 #
+# просто попробую сделать угадай число с "тепло/холодно"
+from random import *
+value = int(input("Попробуйте угадать число от 1 до 100: "))
+number = randint(1, 100)
+diff = number - value
+while True:
+    if value >= 1 and value <=100:
+        while abs(diff) != 0:
+            if abs(diff) >= 1 and abs(diff) <= 10:
+                value = int(input("Очень горячо. Попробуйте угадать еще раз: "))
+                diff = number - value
+            elif abs(diff) > 10 and abs(diff) <= 20:
+                value = int(input("Горячо. Попробуйте угадать еще раз: "))
+                diff = number - value
+            elif abs(diff) > 20 and abs(diff) <= 30:
+                value = int(input("Прохладно. Попробуйте угадать еще раз: "))
+                diff = number - value
+            elif abs(diff) > 30 and abs(diff) <= 40:
+                value = int(input("Холодно. Попробуйте угадать еще раз: "))
+                diff = number - value
+            elif abs(diff) > 40:
+                value = int(input("Очень холодно. Попробуйте угадать еще раз: "))
+                diff = number - value
+        if diff == 0:
+            print("Поздравляю, вы угадали!")
+            break
+    else:
+        while value <= 0 or value >= 100:
+            value = int(input("Введенное вами число не находится в диапазоне от 0 до 100. Введите число еще раз: "))
+            diff = number - value
